3b512821e49c147f71c40346f1f4d7fccd53ace6
Cayle <calebabhulimhen@gmail.com>
2021-02-26 12:48:59 -0600

    Completes the unit 2 of the application, that includes the collection view and a detailed view of each movie.

---
+++ flixApp/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixApp
+//
+//  Created by Caleb Abhulimhen on 2/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
